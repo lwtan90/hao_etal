27f8f7634e42a65b7ad5e3da891bf4917b51ce0e
wlwtan90 <lwtan90@gmail.com>
2022-09-06 16:29:09 -0700

    findMARKER.R: perform differential marker analysis

--- bin/R/findMARKER.R
+++ bin/R/findMARKER.R
@@ -15,14 +15,16 @@ seuOBJ = readRDS("SCT.prepped.RDS")
 
 ### This is the user argument for which cluster is compared here
 args = commandArgs(TRUE)
+celltype = args[1]
+
 ## a quick check to see if the Ident of the cells have been set correctly
 table(Idents(seuOBJ))
 
 ## Performing differential gene expression analysis between the Ident of interest with the rest. (One vs All comparison)
 ## Default test is Wilcox. Only showing only.pos (upregulated in the cluster of interest)
 ## To save time, only focus on those gene expressed at leats 20% of the cluster of interest with logfc of 0.2 (edit table)
-seuOBJ.markers <- FindMarkers(seuOBJ, assay="SCT", ident.1 = args[1], min.pct = 0.2, logfc.threshold = 0.2, only.pos=TRUE)
+seuOBJ.markers <- FindMarkers(seuOBJ, assay="SCT", ident.1 = celltype, min.pct = 0.2, logfc.threshold = 0.2, only.pos=TRUE)
 
 ## output the de output as a text file
-write.table(seuOBJ.markers,file=paste("marker_",args[1],".txt",sep=""),sep="\t",quote=FALSE)
+write.table(seuOBJ.markers,file=paste("marker_",celltype,".txt",sep=""),sep="\t",quote=FALSE)
 
